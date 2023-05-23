c0ac4f53850aa4c17c4b59886782ef3e1aab9997
georgeif2212 <jinfante2212@gmail.com>
2023-05-23 16:21:04 -0600

    Se realizó modificación al algoritmo para saber si es número primo

--- P3_CUDA/xCuenta_primos_cpu.c
+++ P3_CUDA/xCuenta_primos_cpu.c
@@ -17,14 +17,14 @@
 /* Prototipo de la función para que un hilo sume */
 void contarPrimos(void *ptr);
 bool esPrimo(long n);
-void llenarArreglo(int* arr, int tam);
-void printArreglo(int* arr, int tam);
+void llenarArreglo(int *arr, int tam);
+void printArreglo(int *arr, int tam);
 
-#define N (4 * 1024)
+#define N (10000000)
 
 /* Arreglo que tendrá los enteros que queremos verificar si son primos o no */
-int* enteros;
-int* siNo;
+int *enteros;
+int *siNo;
 
 /* Esta es una variable compartida por todos los hilos */
 long long numPrimos = 0;
@@ -33,101 +33,105 @@ int numHilos;
 
 int main(int argc, char const *argv[])
 {
-    /* Inicializar la semilla para generar números aleatorios. */
-    srand(time(NULL));
+   /* Inicializar la semilla para generar números aleatorios. */
+   srand(time(NULL));
 
    /* Verificar que venga como argumento el número de hilos. */
-   if (argc < 2) { // Si pasaron menos de 2 argumentos  
+   if (argc < 2)
+   { // Si pasaron menos de 2 argumentos
       printf("\nDebes correr este programa así:\n");
       printf("\n > %s NUM_HILOS\n", argv[0]);
-      
+
       return 0;
    }
-   
+
    sscanf(argv[1], "%d", &numHilos);
-   
+
    /* Crear los arreglos de enteros y de resultados (sí/no). */
-   enteros = (int *) malloc(N * sizeof(int));
-   siNo = (int*) malloc(N * sizeof(int));
+   enteros = (int *)malloc(N * sizeof(int));
+   siNo = (int *)malloc(N * sizeof(int));
 
    llenarArreglo(enteros, N);
 
    /* Deben declarar un arreglo para manejar los hilos de tipo  pthread_t */
    pthread_t threads[numHilos];
-         
+
    /* Crear los numHilos hilos */
    int id[numHilos];
 
    printf("\nHay %d hilos.\n", numHilos);
-   for (int i = 0; i < numHilos; i++) {
+   for (int i = 0; i < numHilos; i++)
+   {
       id[i] = i;
-      pthread_create(&threads[i], NULL, (void *) contarPrimos, &id[i]);
+      pthread_create(&threads[i], NULL, (void *)contarPrimos, &id[i]);
    }
 
    /* El hilo principal debe mostrar el conteo final */
    printf("\nCalculando resultados...\n\n");
-   
+
    for (int i = 0; i < numHilos; i++)
       pthread_join(threads[i], NULL);
 
-   printf("\nArreglo enteros\n");
-   printArreglo(enteros, N);
-   printf("\nYa terminaron los hilos de identificar números primos.\n\n", numPrimos);
-   printf("\nArreglo SiNo\n");
-   printArreglo(siNo, N);
+   // printf("\nArreglo enteros\n");
+   // printArreglo(enteros, N);
+   // printf("\nYa terminaron los hilos de identificar números primos.\n\n", numPrimos);
+   // printf("\nArreglo SiNo\n");
+   // printArreglo(siNo, N);
 
-    int suma = 0;
-    for (int i=0; i < N; i++)
-        suma += siNo[i];
+   int suma = 0;
+   for (int i = 0; i < N; i++)
+      suma += siNo[i];
 
-    printf("\nHay %d primos\n\n", suma);
+   printf("\nHay %d primos\n\n", suma);
 
    free(enteros);
    free(siNo);
-   
+
    return 0;
 }
 
 void contarPrimos(void *ptrID)
 {
    /* Tomar identificador del hilo actual */
-   int tid = *( (int *) ptrID );
-   
+   int tid = *((int *)ptrID);
+
    while (tid < N)
    {
-      if (esPrimo( enteros[tid] ) == true)
+      if (esPrimo(enteros[tid]) == true)
          siNo[tid] = true;
       else
          siNo[tid] = false;
-      
+
       tid += numHilos; // numHilos es global en este programa.
    }
-   
 }
 
-bool esPrimo(long n) 
+bool esPrimo(long n)
 {
-   bool primo = true;
-   
-   long p = 2;
-   while (primo == true && p < n-1) {
-      if (n % p == 0)
-         primo = false;
-      
-      p++;
+   if (n % 3 == 0)
+      return false;
+
+   for (int i = 5; i * i <= n; i = i + 6)
+   {
+      if (n % i == 0 || n % (i + 2) == 0)
+      {
+         return false;
+      }
    }
-   
-   return primo;
+   return true;
 }
 
-void llenarArreglo(int* arr, int tam) {
-    for (int i=0; i < tam; i++) {
-        arr[i] = 10000000 + rand()%10000;
-        arr[i] = 2*arr[i] + 1;  // Para que el entero sea IMPAR.
-    }
+void llenarArreglo(int *arr, int tam)
+{
+   for (int i = 0; i < tam; i++)
+   {
+      arr[i] = 10000000 + rand() % 10000;
+      arr[i] = 2 * arr[i] + 1; // Para que el entero sea IMPAR.
+   }
 }
 
-void printArreglo(int* arr, int tam) {
-    for (int i=0; i < tam; i++)
-        printf("%d ", arr[i]);
+void printArreglo(int *arr, int tam)
+{
+   for (int i = 0; i < tam; i++)
+      printf("%d ", arr[i]);
 }
